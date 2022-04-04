20307cfcfdb250228e75ec832ab79e516f01053d
incluitnelsoncampos <nelson.campos@incluit.com>
2022-04-04 19:51:57 -0300

    Ejemplo sobre Hooks

---
+++ cypress/integration/clase-3/hooks.js
@@ -0,0 +1,19 @@
+/// <reference types="cypress" />
+
+before(() => {
+    cy.log('Casa')
+})
+
+describe('Ejemplos Hooks', () => {
+    beforeEach(() => {
+        cy.log('Perro')
+    })
+
+    it('Prueba 1', () => {
+
+    })
+
+    it('Prueba 2', () => {
+        
+    })
+})
